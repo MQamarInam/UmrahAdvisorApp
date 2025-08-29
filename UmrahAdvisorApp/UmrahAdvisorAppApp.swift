b35d236c99f738f035185deb0069fdf17a568de8
MQamarInam <qamarinam028@gmail.com>
2025-08-29 22:53:07 +0500

    Initial Commit

---
+++ UmrahAdvisorApp/UmrahAdvisorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmrahAdvisorAppApp.swift
+//  UmrahAdvisorApp
+//
+//  Created by Qaim's Macbook  on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct UmrahAdvisorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
