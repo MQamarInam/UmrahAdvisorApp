62022838fc6209e2db61eadd21dccf921e823684
MQamarInam <qamarinam028@gmail.com>
2025-11-10 12:02:39 +0500

    Added Enum for PackageCategory

---
+++ UmrahAdvisorApp/Enum/PackageCategory.swift
@@ -0,0 +1,16 @@
+//
+//  PackageCategory.swift
+//  UmrahAdvisorApp
+//
+//  Created by Qaim's Macbook  on 10/11/2025.
+//
+
+import Foundation
+
+enum PackageCategory: String, CaseIterable {
+    case all = "All"
+    case hot = "Hot"
+    case days21 = "21 Days"
+    case days14 = "14 Days"
+    case days7 = "7 Days"
+}
